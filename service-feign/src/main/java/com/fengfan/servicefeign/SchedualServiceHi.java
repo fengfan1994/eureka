5e7d5d1b202268520df1ee6958351c70e99881d0
fengfan <15071148197@163.com>
2020-03-26 15:25:31 +0800

    Initial commit

---
+++ service-feign/src/main/java/com/fengfan/servicefeign/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.fengfan.servicefeign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//指定调用服务的名字，一定是注册表中的名字
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
